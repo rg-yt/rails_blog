833543f1123d5b11aa6db777388edd3bb5513740
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-12-17 15:24:37 -0500

    Update routes to make comments a nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
   root 'articles#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
